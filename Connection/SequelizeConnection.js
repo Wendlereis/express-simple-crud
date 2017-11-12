f3d71c329c1ef5838ba03340f30d00afdc16bc34
Wendler Eis <wendler_eis@hotmail.com>
2017-11-12 21:26:09 -0200

    Added:  Environment variables

--- Connection/SequelizeConnection.js
+++ Connection/SequelizeConnection.js
@@ -1,10 +1,10 @@
 //Using Sequelize (ORM for Postgress)
 const Sequelize = require('sequelize')
 
-const host = ''
-const database = ''
-const username = ''
-const password = ''
+const host = process.env.HOST
+const database = process.env.DB
+const username = process.env.USR
+const password = process.env.PASS
 
 module.exports = new Sequelize(database, username, password, {
     host: host,
